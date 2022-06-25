ebae7faf8ad66293ad939a6258a8250dc248b6f4
cfan2020 <2762179987@qq.com>
2022-06-25 15:06:58 +0800

    在Student类中修改age-age2

--- TestModule01/src/com/msb/Student.java
+++ TestModule01/src/com/msb/Student.java
@@ -5,7 +5,7 @@ package com.msb;
  * @Date: 2022/6/23 - 06 - 23 - 17:40
  */
 public class Student {
-    private int age;
+    private int age2;
     private String name;
     private String sss;
 }
